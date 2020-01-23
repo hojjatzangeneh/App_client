1c32aefc521c8a2fe04d4f2abb9fa703b08f35bd
Zangeneh <hr.zangeneh@yahoo.com>
2020-01-23 12:23:43 +0330

    .

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,10 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
+    <v-app-bar app color="primary" dark>
       <div class="d-flex align-center">
         <v-img
           alt="Vuetify Logo"
@@ -27,34 +23,40 @@
 
       <v-spacer></v-spacer>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
+      <v-btn href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank" text>
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <HelloWorld />
+      <div>
+        <v-alert v-if="type=='success'" type="success">I'm a success alert.</v-alert>
+
+        <v-alert v-if="type=='info'"  type="info">I'm an info alert.</v-alert>
+
+        <v-alert v-if="type=='warning'"  type="warning">I'm a warning alert.</v-alert>
+
+        <v-alert v-if="type=='error'"  type="error">I'm an error alert.</v-alert>
+      </div>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    HelloWorld
   },
 
   data: () => ({
-    //
-  }),
-};
+     type : "error"
+  })
+  };
+
 </script>
